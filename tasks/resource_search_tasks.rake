40bc8f81b33a398246a98fc0ae5c8f74f695bd1c
Graeme Mathieson <mathie@woss.name>
2007-07-22 20:39:24 +0000

    Remove unused files (for now).
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/resource_search@120 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- tasks/resource_search_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :resource_search do
-#   # Task goes here
-# end
--- test/resource_search_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test/unit'
-
-class ResourceSearchTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
-  end
-end
