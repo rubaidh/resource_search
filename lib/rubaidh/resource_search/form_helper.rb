f99a44bf45e54ff7a1e2d52e7f3ac48b1fbd5934
Graeme Mathieson <mathie@woss.name>
2007-07-23 19:33:52 +0000

    Basic AJAX version of the searching.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/resource_search@129 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- lib/rubaidh/resource_search/form_helper.rb
+++ lib/rubaidh/resource_search/form_helper.rb
@@ -16,6 +16,17 @@ module Rubaidh
             }.merge(options[:html] || {})
           }.merge(options), &block)
         end
+        
+        def remote_search_form_for(name, options = {}, &block)
+          remote_form_for(name,{
+            :url => send("#{name.to_s.pluralize}_path"),
+            :method => :get,
+            :html => {
+              :class => "search_#{name}",
+              :id => "search_#{name}",
+            }.merge(options[:html] || {})
+          }.merge(options), &block)
+        end
       end
     end
   end
