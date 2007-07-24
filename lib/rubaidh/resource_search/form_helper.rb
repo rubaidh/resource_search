bc8e9e97cf86dc9c4d240536320a37fb6baed583
Graeme Mathieson <mathie@woss.name>
2007-07-24 07:45:09 +0000

    Provide an observer too, if desired.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/resource_search@136 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- lib/rubaidh/resource_search/form_helper.rb
+++ lib/rubaidh/resource_search/form_helper.rb
@@ -18,14 +18,26 @@ module Rubaidh
         end
         
         def remote_search_form_for(name, options = {}, &block)
+          observer = options.delete(:observer)
+          class_and_id = "search_#{name}"
+          path = send("#{name.to_s.pluralize}_path")
+
           remote_form_for(name,{
-            :url => send("#{name.to_s.pluralize}_path"),
+            :url => path,
             :method => :get,
             :html => {
-              :class => "search_#{name}",
-              :id => "search_#{name}",
+              :class => class_and_id,
+              :id => class_and_id,
             }.merge(options[:html] || {})
           }.merge(options), &block)
+
+          unless observer.nil?
+            concat observe_field("#{name}_#{observer}",
+              :frequency => 0.5,
+              :url => path,
+              :with => "Form.serialize('#{class_and_id}')",
+              :method => :get), block.binding
+          end
         end
       end
     end
