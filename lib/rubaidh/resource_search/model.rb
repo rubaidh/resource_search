345a532dc9178b86525db892fffafe6424f4679b
Graeme Mathieson <mathie@woss.name>
2008-01-17 15:08:49 +0000

    Lets write some specs before we try refactoring, eh?
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/resource_search@231 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- lib/rubaidh/resource_search/model.rb
+++ lib/rubaidh/resource_search/model.rb
@@ -2,21 +2,17 @@ module Rubaidh
   module ResourceSearch
     module Model
       module ClassMethods
-        def with_search_terms(params, &block)
-          with_scope( { :find => resource_search_scope(params) }, &block)
-        end
-        
-        private
-        def resource_search_scope(params)
+        def with_search_terms(params)
           conditions = params.blank? ? [] : params.delete_if do |key, value|
            !column_names.include?(key)
           end.collect do |key, value|
            ["LOWER(#{table_name}.#{key}) LIKE ?", "%#{value.downcase}%"]
           end.unzip
-
           conditions.length > 1 && conditions[0] = conditions[0].join(' AND ')
-
-          { :conditions => conditions.flatten }
+          conditions = conditions.flatten
+          with_scope :find => {:conditions => conditions.empty? ? nil : conditions} do
+            yield
+          end
         end
       end
     end
