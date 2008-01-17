a8231507764b4845ff5e4cb90fef5f326df7ef3e
Graeme Mathieson <mathie@woss.name>
2008-01-17 13:00:33 +0000

    Spec helper to pull in the surrounding rails application.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/resource_search@226 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
-require 'rubygems'
-gem 'activesupport'
-require 'active_support'
-
-$:.unshift("#{File.dirname(__FILE__)}/../lib")
+# Pull a spec_helper from the hosting Rails application.
+# FIXME: At some point it would be nice not to need the plugin to be
+# part of a Rails application in order to test it...
+require "#{File.dirname(__FILE__)}/../../../../spec/spec_helper"
